039628cb37d675fb488b0f6f2e9beb899f2161c3
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-03-06 14:30:24 +0300

    memory leak

--- agent/helpers/metric/insert.lua
+++ agent/helpers/metric/insert.lua
@@ -1,8 +1,13 @@
+local count = 0
+
 local function insert(host, plugin, snapshot, value_bigint, value_double, value_jsonb, manager)
   local stmt, err = manager:stmt('select agent.insert_metric($1::text, $2::text, $3::bigint, $4::bigint, $5::float8, $6::jsonb)')
   if err then error(err) end
   local _, err = stmt:exec(host, plugin, snapshot, value_bigint, value_double, value_jsonb)
   if err then error("exec error: "..err) end
+  -- clear plans cache
+  count = count + 1
+  if count % 10 == 0 then manager:exec('DISCARD PLANS') end
 end
 
 return insert
