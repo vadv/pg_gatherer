234142d77eccb1941378838faaa74d4ab596bd65
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-03-12 11:09:09 +0300

    telegram alerts

--- alertmanager/sender/telegram.lua
+++ alertmanager/sender/telegram.lua
@@ -1,4 +1,7 @@
 local time = require("time")
+local telegram = require("telegram")
+local http = require("http")
+local humanize = require("humanize")
 
 local helpers = dofile(os.getenv("CONFIG_INIT"))
 local manager = helpers.connections.manager
@@ -13,13 +16,44 @@ if not(os.getenv("TELEGRAM_ENABLED") == "true") then
   end
 end
 
-local stmt, err = manager:stmt("select key, info from manager.alert where host = $1")
+local client = http.client({})
+local telegram_bot = telegram.bot(os.getenv("TELEGRAM_TOKEN"), client)
+local telegram_chat = os.getenv("TELEGRAM_CHAT")
+
+local cache = {
+  -- host+key = { created_at = time, notify_at = time }
+}
+local cache_counter = 0
+function cache.get(host, key)
+  -- body
+end
+
+local stmt, err = manager:stmt("select key, info, created_at from manager.alert where host = $1")
 if err then error(err) end
 
 function collect()
   for _, host in pairs(get_hosts()) do
-   --
+    local result, err = stmt:query(host)
+    if err then error(err) end
+    for _, row in pairs(result.rows) do
+
+      -- format message
+      local message = [[
+Host:    %s
+Problem: %s
+Age:     %s
+]]
+      message = string.format(message, host, row[1], humanize.time(row[3]))
+
+      -- send message
+      local _, err = telegram_bot:sendMessage({
+        chat_id = telegram_chat,
+        text = message
+      })
+      if err then error(err) end
+
+    end
   end
 end
 
-helpers.runner.run_every(collect, 5)
+helpers.runner.run_every(collect, 10)
