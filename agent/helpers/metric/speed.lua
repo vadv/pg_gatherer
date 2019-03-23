74d9a05c510413579a4be9d35b7cc872e2ed3cd7
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-03-23 23:03:04 +0300

    badger -> sqlite

--- agent/helpers/metric/speed.lua
+++ agent/helpers/metric/speed.lua
@@ -1,14 +1,87 @@
 local time = require("time")
-local storage = require("storage")
-local crypto = require("crypto")
+local db = require("db")
+local filepath = require("filepath")
 
-local db, err = storage.open(os.getenv("CACHE_PATH"), "badger")
+local create_table_every_sec = 60*60
+local gc_sqlite_after_save_count = 1000
+
+local sqlite, err = db.open("sqlite3", filepath.join(os.getenv("CACHE_PATH"), "speed.sqlite3"), {shared = true})
 if err then error(err) end
 
-local function save(key, value)
-  local data = {value = value, unixts = time.unix()}
-  local err = db:set(key, data, 5*60)
+local _, err = sqlite:command("PRAGMA synchronous = 0;")
+if err then error(err) end
+
+local _, err = sqlite:command("PRAGMA journal_mode = OFF;")
+if err then error(err) end
+
+local created_tables = {
+  -- table_name = {
+--    save_stmt = ud,
+--    get_stmt = ud,
+--  }
+}
+
+local function table_name_ts()
+  local now = time.unix()
+  local table_ts = now - (now%(create_table_every_sec))
+  return "speed_"..tostring(table_ts)
+end
+
+local function get_and_check_table()
+  local table_name_ts = table_name_ts()
+  if created_tables[table_name_ts] then return table_name_ts end
+  local _, err = sqlite:exec("create table if not exists " .. table_name_ts .. " (key text primary key, value real, updated_at real);")
   if err then error(err) end
+
+  local get_stmt, err = sqlite:stmt("select value, updated_at from " .. table_name_ts .. " where key = ?")
+  if err then error(err) end
+  local save_stmt, err = sqlite:stmt("insert into ".. table_name_ts .. " (key, value, updated_at) values (?, ?, ?) on conflict (key) do update set value=excluded.value, updated_at=excluded.updated_at")
+  if err then error(err) end
+
+  created_tables[table_name_ts] = {
+    get_stmt = get_stmt,
+    save_stmt= save_stmt
+  }
+  return table_name_ts
+end
+
+-- drop old tables
+local counter = 0
+local function gc()
+  counter = counter + 1
+  if counter > gc_sqlite_after_save_count then
+    local table_name_ts = table_name_ts()
+    for k, _ in pairs( created_tables ) do
+      if not(k == table_name_ts) then
+        print("sqlite: drop table "..k)
+        local _, err = sqlite:exec("drop table if exists "..k..";")
+        if err then error(err) end
+        created_tables[k] = nil
+      end
+    end
+    conter = 0
+  end
+end
+
+-- save
+local function save(key, value)
+  local table_name = get_and_check_table()
+  local save_stmt = created_tables[table_name].save_stmt
+  save_stmt:exec(key, value, time.unix())
+  if err then error(err) end
+  gc()
+end
+
+-- value, updated_at, found
+local function get(key)
+  local table_name = get_and_check_table()
+  local get_stmt = created_tables[table_name].get_stmt
+  local result, err = get_stmt:query(key)
+  if err then error(err) end
+  if result.rows and result.rows[1] then
+    return result.rows[1][1], result.rows[1][2], true
+  end
+  return nil, nil, false
 end
 
 local function speed(key, value)
@@ -16,27 +89,25 @@ local function speed(key, value)
   if not value then return nil end
 
   local now = time.unix()
-  local hash_key = crypto.md5(key)
 
-  local prev, found, err = db:get(hash_key)
-  if err then error(err) end
+  local prev, updated_at, found = get(key)
   if not found then
-    save(hash_key, value)
+    save(key, value)
     return
   end
 
   -- overflow
-  if prev.value > value then
-    save(hash_key, value)
+  if prev > value then
+    save(key, value)
     return
   end
 
 
   -- calc
-  local time_diff = now - prev.unixts
-  local value_diff = value - prev.value
+  local time_diff = now - updated_at
+  local value_diff = value - prev
 
-  save(hash_key, value)
+  save(key, value)
 
   -- value
   return (value_diff / time_diff)
