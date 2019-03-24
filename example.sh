18ebd08080c92ce7cbe0f18ba9a042800de97506
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-03-24 19:19:15 +0300

    small fixes

--- example.sh
+++ example.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
-pushd `dirname "$(readlink -f "$0")"`
+pushd $(dirname "$(readlink -f "$0")")
 docker build -t pg_gatherer:latest -f Dockerfile.example .
 docker run --rm -t -v $(pwd):/app -i pg_gatherer bash
