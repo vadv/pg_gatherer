0b6596199a8708128dc6fd7d5bec50827e313575
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-04-03 16:24:01 +0300

    add wraparound checker

---
+++ alertmanager/generator/wraparound.lua
@@ -0,0 +1,62 @@
+local json = require("json")
+local inspect = require("inspect")
+local helpers = dofile(os.getenv("CONFIG_INIT"))
+
+local manager = helpers.connections.manager
+local function get_hosts()
+  return helpers.query.get_hosts(helpers.connections.manager)
+end
+local function create_alert(host, key, severity, info)
+  helpers.query.create_alert(host, key, severity, info, helpers.connections.manager)
+end
+local function resolve_alert(host, key)
+  helpers.query.resolve_alert(host, key, helpers.connections.manager)
+end
+local function unixts()
+  return helpers.query.unixts(helpers.connections.manager)
+end
+
+local alert_key = "prevent database wraparound"
+
+local stmt, err = manager:stmt([[
+  select
+    value_jsonb->>'datname' as "datname",
+    coalesce((value_jsonb->>'age')::bigint, 0) as "age"
+  from
+    manager.metric
+  where
+    host = md5($1::text)::uuid
+    and plugin = md5('pg.databases')::uuid
+    and ts > ($2 - 10 * 60)
+    and ts < $2
+    and coalesce((value_jsonb->>'age')::bigint, 0) > 300 * 1000 * 1000
+  order by 2 desc
+  limit 1
+]])
+
+if err then error(err) end
+
+function collect()
+
+  local current_unixts = unixts()
+
+  for _, host in pairs(get_hosts()) do
+
+    local result, err = stmt:query(host, current_unixts)
+    if err then error(err) end
+
+    if not(result.rows[1] == nil) and not(result.rows[1][1] == nil) then
+      local database, age = result.rows[1][1], result.rows[1][2]
+      -- alert
+      local jsonb = {custom_details={database=database, age=age}}
+      local jsonb, err = json.encode(jsonb)
+      if err then error(err) end
+      create_alert(host, alert_key, 'critical', jsonb)
+    else
+      resolve_alert(host, alert_key)
+    end
+
+  end
+end
+
+helpers.runner.run_every(collect, 60)
