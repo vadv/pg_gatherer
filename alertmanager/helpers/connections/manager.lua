e07f91668df3cb6e760e748f0eb8a46029c026f8
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-09-13 18:23:16 +0300

    clean

--- alertmanager/helpers/connections/manager.lua
+++
@@ -1,4 +0,0 @@
-local db = require("db")
-local manager, err = db.open("postgres", os.getenv("MANAGER"), {shared=true})
-if err then error(err) end
-return manager
--- example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -ex
-pushd $(dirname "$(readlink -f "$0")")
-docker build -t pg_gatherer:latest -f Dockerfile.example .
-docker run --rm -t -v $(pwd):/app -i pg_gatherer bash
