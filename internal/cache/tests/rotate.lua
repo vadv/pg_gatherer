371e9b1095474792a958e10b8d5e5d68b7c0ebc7
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-09-16 14:25:39 +0300

    fix tests

--- internal/cache/tests/rotate.lua
+++ internal/cache/tests/rotate.lua
@@ -6,11 +6,9 @@ local err = cache:set("must_be_rotated_after_2_second", 1)
 if err then error(err) end
 
 time.sleep(1.5)
-local value, err = cache:get("must_be_rotated_after_2_second")
-if err then error(err) end
+local value = cache:get("must_be_rotated_after_2_second")
 if not(value == 1) then error("must be get from prev table") end
 
 time.sleep(3)
-local value, err = cache:get("must_be_rotated_after_2_second")
-if err then error(err) end
+local value = cache:get("must_be_rotated_after_2_second")
 if value then error("table must be rotated") end
