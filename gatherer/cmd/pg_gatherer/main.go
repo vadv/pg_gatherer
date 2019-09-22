e9ffe210d2c48476884f7cfa2a5ccce9406313f6
vadv <vadv.mkn@gmail.com>
2019-09-22 10:17:37 +0300

    Prometheus (#6)

--- gatherer/cmd/pg_gatherer/main.go
+++ gatherer/cmd/pg_gatherer/main.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/vadv/pg_gatherer/gatherer/internal/plugins"
 
@@ -15,9 +18,12 @@ import (
 )
 
 var (
-	version     = `unknown`
-	configPath  = flag.String(`config`, `config.yaml`, `Path to config file`)
-	versionFlag = flag.Bool(`version`, false, `Print version and exit`)
+	version        = `unknown`
+	hostConfigFile = flag.String(`host-config`, `host.yaml`, `Path to config file with host configurations.`)
+	pluginDir      = flag.String(`plugins`, `./plugins`, `Path to plugins directory`)
+	cacheDir       = flag.String(`cache`, `./cache`, `Path to cache directory`)
+	httpListen     = flag.String(`http-listen`, `:8080`, `Lister address`)
+	versionFlag    = flag.Bool(`version`, false, `Print version and exit`)
 )
 
 func main() {
@@ -31,14 +37,14 @@ func main() {
 		os.Exit(0)
 	}
 
-	data, err := ioutil.ReadFile(*configPath)
+	data, err := ioutil.ReadFile(*hostConfigFile)
 	if err != nil {
-		log.Printf("[FATAL] read config '%s': %s\n", *configPath, err.Error())
+		log.Printf("[FATAL] read config '%s': %s\n", *hostConfigFile, err.Error())
 		os.Exit(1)
 	}
-	config := &Config{}
-	if errMarshal := yaml.Unmarshal(data, config); errMarshal != nil {
-		log.Printf("[FATAL] parse config '%s': %s\n", *configPath, errMarshal.Error())
+	config := new(Config)
+	if errMarshal := yaml.Unmarshal(data, &config); errMarshal != nil {
+		log.Printf("[FATAL] parse config '%s': %s\n", *hostConfigFile, errMarshal.Error())
 		os.Exit(1)
 	}
 	if errConfig := config.validate(); errConfig != nil {
@@ -46,8 +52,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	pool := plugins.NewPool(config.PluginsDir, config.CacheDir)
-	for host, hostConfig := range config.Hosts {
+	// http
+	m := http.NewServeMux()
+	httpServer := &http.Server{Addr: *httpListen, Handler: m}
+	m.Handle("/", http.RedirectHandler("/metrics", http.StatusFound))
+	m.Handle("/metrics", promhttp.Handler())
+	go func() {
+		if errListen := httpServer.ListenAndServe(); errListen != nil {
+			log.Printf("[FATAL] http listen: %s\n", errListen.Error())
+			os.Exit(2)
+		}
+	}()
+
+	pool := plugins.NewPool(*pluginDir, *cacheDir)
+	for host, hostConfig := range *config {
 		log.Printf("[INFO] register host: '%s'\n", host)
 		pool.RegisterHost(host, hostConfig.Connections)
 		for _, pl := range hostConfig.Plugins {
@@ -55,10 +73,11 @@ func main() {
 			if errPl := pool.AddPluginToHost(pl, host); errPl != nil {
 				log.Printf("[FATAL] register plugin '%s' for host '%s': %s\n",
 					pl, host, errPl.Error())
-				os.Exit(2)
+				os.Exit(3)
 			}
 		}
 	}
+	go prometheusCollectPoolStatistics(pool)
 	log.Printf("[INFO] started\n")
 
 	sig := make(chan os.Signal, 1)
@@ -66,7 +85,7 @@ func main() {
 	<-sig
 
 	log.Printf("[INFO] shutdown\n")
-	for host, _ := range config.Hosts {
+	for host, _ := range *config {
 		pool.RemoveHostAndPlugins(host)
 	}
 	log.Printf("[INFO] stopped\n")
