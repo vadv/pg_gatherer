f463da3b4527eadaf1654d360e89625246f85763
Dmitry Vasilyev <vadv.mkn@gmail.com>
2019-09-23 16:32:47 +0300

    fix diskstat

--- plugins/linux_diskstats/helper_disk_stat.lua
+++ plugins/linux_diskstats/helper_disk_stat.lua
@@ -12,15 +12,17 @@ local function read_diskstat()
     local major, minor, dev_name,
     rd_ios, rd_merges_or_rd_sec, rd_sec_or_wr_ios, rd_ticks_or_wr_sec,
     wr_ios, wr_merges, wr_sec, wr_ticks, ios_pgr, tot_ticks, rq_ticks = line:match(pattern)
-    result[dev_name]                                                  = {
-      major            = tonumber(major), minor = tonumber(minor),
-      rd_ios           = tonumber(rd_ios), rd_merges_or_rd_sec = tonumber(rd_merges_or_rd_sec),
-      rd_sec_or_wr_ios = tonumber(rd_sec_or_wr_ios), rd_ticks_or_wr_sec = tonumber(rd_ticks_or_wr_sec),
-      wr_ios           = tonumber(wr_ios), wr_merges = tonumber(wr_merges),
-      wr_sec           = tonumber(wr_sec), wr_ticks = tonumber(wr_ticks),
-      ios_pgr          = tonumber(ios_pgr), tot_ticks = tonumber(tot_ticks),
-      rq_ticks         = tonumber(rq_ticks)
-    }
+    if dev_name then
+      result[dev_name]                                                  = {
+        major            = tonumber(major), minor = tonumber(minor),
+        rd_ios           = tonumber(rd_ios), rd_merges_or_rd_sec = tonumber(rd_merges_or_rd_sec),
+        rd_sec_or_wr_ios = tonumber(rd_sec_or_wr_ios), rd_ticks_or_wr_sec = tonumber(rd_ticks_or_wr_sec),
+        wr_ios           = tonumber(wr_ios), wr_merges = tonumber(wr_merges),
+        wr_sec           = tonumber(wr_sec), wr_ticks = tonumber(wr_ticks),
+        ios_pgr          = tonumber(ios_pgr), tot_ticks = tonumber(tot_ticks),
+        rq_ticks         = tonumber(rq_ticks)
+      }
+    end
   end
   return result
 end
