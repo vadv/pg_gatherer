34258771d1d52b997ca8161a43ae078fe3499e59
vadv <vadv.mkn@gmail.com>
2019-09-23 14:48:55 +0300

    Fix disk-stat plugin (#12)

--- plugins/linux_diskstats/test.lua
+++ plugins/linux_diskstats/test.lua
@@ -1,6 +1,19 @@
-if not goos.stat(HOST_PROC_DIR..'/diskstats') then
+if not goos.stat(HOST_PROC_DIR .. '/diskstats') then
   print('disabled plugin, because /proc/diskstats not found')
   return
+end
+
+if os.getenv('CI') then
+  local helper = dofile(filepath.join(plugin:dir(), "helper_disk_stat.lua"))
+  local count = 0
+  for dev, value in pairs(helper.read_diskstat()) do
+    print("dev:", dev, "value:", value)
+    count = count + 1
+  end
+  if count == 0 then
+    print('disabled plugin: diskstat is empty')
+    return
+  end
 end
 
 run_plugin_test(120, function() return metric_exists('linux.diskstats') end)
