e73f5e9dd3798a56c0657defc31671d02e590160
Denis Filippov <dfilippov@oneincsystems.com>
2020-03-02 20:26:51 +0700

    Fixed plugin for btree_bloat

--- plugins/btree_bloat/plugin.lua
+++ plugins/btree_bloat/plugin.lua
@@ -6,13 +6,7 @@ local sql_btree_bloat = read_file_in_plugin_dir("btree_bloat.sql")
 local function collect()
   local result = target:query(sql_btree_bloat, 60)
   for _, row in pairs(result.rows) do
-    local jsonb, err = json.decode(row[2])
-    if err then error(err) end
-    local id     = jsonb.id
-
-    local jsonb, err     = json.encode(jsonb)
-    if err then error(err) end
-    storage_insert_metric({ plugin = plugin_name, snapshot = row[1], json = jsonb })
+    storage_insert_metric({ plugin = plugin_name, snapshot = row[1], json = row[2] })
   end
 end
 
