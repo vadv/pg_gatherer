b7d438a91da23188526c6a90691ecefdc190c43b
Dmitry Vasiliev <vadv.mkn@gmail.com>
2020-07-22 22:56:53 +0300

    fixes

--- gatherer/cmd/testing/main.go
+++ gatherer/cmd/testing/main.go
@@ -14,9 +14,8 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
-	"github.com/vadv/pg_gatherer/gatherer/internal/connection"
-
 	libs "github.com/vadv/gopher-lua-libs"
+	"github.com/vadv/pg_gatherer/gatherer/internal/connection"
 	"github.com/vadv/pg_gatherer/gatherer/internal/testing_framework"
 	lua "github.com/yuin/gopher-lua"
 )
